953e978e14c6bae9bee0a19be22e18120dcd58b6
johani23 <ahmad.a.johani@gmail.com>
2025-06-24 15:51:35 +0300

    Delete pairwise_model_selector.py

--- pairwise_model_selector.py
+++
@@ -1,8 +0,0 @@
-def select_pairwise_model(cluster_name):
-    return {
-        "The Idealist": "hope_vs_blindness",
-        "The Silent Doubter": "guarded_vs_genuine",
-        "The Burnt Survivor": "trauma_vs_trust",
-        "The Rescuer": "giver_vs_empty",
-        "The Echoed": "repeat_vs_renewal"
-    }.get(cluster_name, "default_model")
